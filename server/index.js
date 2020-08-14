61fca35eb3ca5e84c19d366a6f09d7a5c64d0dee
Amanda Chen <achen@squarespace.com>
2020-08-13 20:23:11 -0400

    add server stuff

--- server/index.js
+++ server/index.js
@@ -5,11 +5,13 @@ const pino = require('express-pino-logger')();
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(pino);
-app.use(bodyParser.json());
 
-
-app.get('/hey', async (req, res) => {
-    const test = ["hi", "truffle"]
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(test));
+app.get('/api/greeting', (req, res) => {
+  const name = req.query.name || 'World';
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
 });
+
+app.listen(3001, () =>
+  console.log('Express server is running on localhost:3001')
+);
--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from "axios";
 import './App.css';
 
 function App() {
+  const [data, setData] = useState();
+
+  const callBackend = () => {
+    axios.get(`/api/greeting`)
+      .then(response => setData(response.data.greeting));
+  }
+
+  callBackend();
+
   return (
     <div className="App">
       <header className="App-header">
         <p>
           POKEMON COACH
         </p>
+        {data}
       </header>
     </div>
   );
