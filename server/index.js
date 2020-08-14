547347470e6c1cdc413f734f5114875435676cba
ollyweiss <olly.weiss@gmail.com>
2020-08-13 20:23:22 -0400

    add dbs

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require("fs");
 const pino = require('express-pino-logger')();
 
 const app = express();
@@ -7,9 +8,24 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(pino);
 app.use(bodyParser.json());
 
+const pokemonTypeFile = fs.readFileSync('./public/types.txt');
+const pokemonTypes = pokemonTypeFile.split('\n');
+
+const pokemonFile = fs.readFileSync('./public/pokemon.csv');
 
 app.get('/hey', async (req, res) => {
-    const test = ["hi", "truffle"]
+    const test = ["hi", "truffle"];
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify(test));
+});
+
+
+app.get('/weaknesses', async (req, res) => {
+    if (pokemonTypes.includes(req.body)) {
+        console.log('wee');
+    }
+
+    // const test = ["hi", "truffle"]
+    // res.setHeader('Content-Type', 'application/json');
+    // res.send(JSON.stringify(test));
 });
